6ae84a751d86bc668049631d32f864dead634551
masongzhi <511071161@qq.com>
2018-07-18 16:58:43 +0800

    补充文档
    修复proxyTable

--- client/config/index.js
+++ client/config/index.js
@@ -12,9 +12,13 @@ module.exports = {
     proxyTable: {
       '/api': {
         target: 'http://localhost:3001',
+        // secure: false,  // 如果是https接口，需要配置这个参数
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
       },
       '/__DEV__': {
         target: 'http://localhost:3001',
+        // secure: false,  // 如果是https接口，需要配置这个参数
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
       },
     },
 
