072b7630bccac0ac3e1882b582a25c6167ca80a2
masongzhi <511071161@qq.com>
2018-07-31 15:49:24 +0800

    修改 example config/index.js 配置

--- example/config/index.js
+++ example/config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     proxyTable: {
       // 填写 .mamockrc.js的prefix，默认为'/__DEV__'
       '/__DEV__': {
-        target: 'http://localhost:3001', // 接口的域名
+        target: 'http://localhost:3002', // 接口的域名
         // secure: false,  // 如果是https接口，需要配置这个参数
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
       }
